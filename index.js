2263414d637dd65aa4c9d53478db1822dd5c6a69
saikiransk027 <saikranale2@gmail.com>
2021-07-21 11:17:08 +0530

    final

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
- //index.js
- 'use strict' ;
+let secretNumber = 20;
+let score = 20;
+let highestRecord = 0;
 
- var secretNumber = math.trunc(Math.random()*20+1);
- const number = document.querySelector('.number');
- 
- 
+document.addEventListener('DOMContentLoaded', function (event) {
+  document.querySelector('.score').textContent = 'Score: ' + score;
+  document.querySelector('.message').textContent = 'Your Guess!';
+  secretNumber = Math.floor(Math.random() * 20 + 1);
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.record').textContent = 'Highest Score: ' + 0;
+  console.log(secretNumber);
+});
 
- 
- const btnCheck = document.querySelector('.check');
- 
-//  console.log(secretNumber);
+document.querySelector('.check-btn').addEventListener('click', function () {
+  let inputNumber = document.querySelector('.guess-number').value;
+  console.log(inputNumber === secretNumber);
+  console.log(secretNumber);
+  if (inputNumber == secretNumber) {
+    document.querySelector('.message').textContent = 'Congratulations!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#62cc67';
+    if (score > highestRecord) {
+      document.querySelector('.record').textContent =
+        'Highest Score: ' + score;
+    }
+    return;
+  } else if (inputNumber < secretNumber) {
+    document.querySelector('.message').textContent = 'You guess is too small!';
+    score--;
+  } else {
+    document.querySelector('.message').textContent = 'You guess is too large!';
+    score--;
+  }
+  document.querySelector('.score').textContent = 'Score: ' + score;
+});
 
-const msg= document.querySelector(.msg)
- btnCheck.addEventListener('click', function(){
-    const guess =Number( document.querySelector('.guess').value);
-   if (guess > 0){
-       console.log(` number is ${guess}`);
-   else{
-           console.log('provide right input');
-
-       }
-       msg.textContent = 'Provide Right Input' ;
-   }
-    // const usrInput = Number(guess.Value);
-   
-    // console.log(usrInput);
-    //  console.log(typeof (usrInput))
- })
+document.querySelector('.again-btn').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.floor(Math.random() * 20 + 1);
+  document.querySelector('.score').textContent = 'Score: ' + score;
+  document.querySelector('.message').textContent = 'Your Guess!';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#CB6D52';
+});
